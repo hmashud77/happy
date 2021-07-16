3443ee948e131ddab110eb3be5eeefdf2aa48437
hmash <hmash@LAPTOP-BE9N6N5S.fios-router.home>
2021-07-15 21:11:33 -0400

    commit by happy

---
+++ Happy1/src/test/java/ha/haha.java
@@ -0,0 +1,18 @@
+package ha;
+
+public class haha {
+
+public static void main(String[] args) {
+	
+System.out.println("Hello");	
+System.out.println("Hi java");
+System.out.println("I love java");
+	
+	
+}	
+	
+	
+	
+	
+	
+}
---
+++ Happy1/src/test/java/ha/hasu.java
@@ -0,0 +1,34 @@
+package ha;
+
+public class hasu {
+
+	public void sam() {
+		
+		System.out.println("love");
+	}
+	
+public void Automation() {
+		
+		System.out.println("Hi");
+		
+	}
+public static void java1() {
+	
+	System.out.println("love");
+	
+}
+public static void main(String[] args){
+	hasu obj = new hasu();
+ 
+	obj.sam();
+	obj.Automation();
+	hasu.java1();
+
+}
+	
+	
+
+}
+
+	
+
